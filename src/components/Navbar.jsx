52d7b779cde944663a166c967af8bf4a94f54348
Muzammil Ishfaq Kahloon <130220559+MuzamilKahloon@users.noreply.github.com>
2024-12-24 15:02:11 +0500

    comment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,7 @@ const Navbar = () => {
   const { t, i18n } = useTranslation(); // Initialize translation hook
   const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
   const [searchOpen, setSearchOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState(""); // Search query state
   const [languageMenuOpen, setLanguageMenuOpen] = useState(false);
 
   // Function to toggle the mobile drawer
@@ -45,111 +46,23 @@ const Navbar = () => {
         </div>
 
         {/* Desktop Navigation Links */}
-        <ul className="hidden space-x-8 lg:flex">
-          {["home", "about", "research", "publications", "cv"].map((key) => (
-            <li key={key}>
-              <Link
-                to={`/${key}`}
-                className="transition hover:text-teal-400"
-              >
-                {capitalizeFirstLetter(t(key))} {/* Capitalize translation */}
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        {/* Icons Section */}
-        <div className="flex items-center space-x-6">
-          {/* Language Selector */}
-          <div className="relative hidden lg:block">
-            <button
-              onClick={() => setLanguageMenuOpen(!languageMenuOpen)}
-              aria-label="Language Selector"
-              className="flex items-center px-3 py-2 space-x-2 bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none"
-            >
-              <FiGlobe size={20} />
-              <span>{t("language") || "Language"}</span>
-            </button>
-            {languageMenuOpen && (
-              <div className="absolute right-0 mt-2 text-black bg-white rounded shadow-lg">
-                <ul>
-                  <li
-                    className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-                    onClick={() => switchLanguage("en")}
-                  >
-                    English
-                  </li>
-                  <li
-                    className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-                    onClick={() => switchLanguage("es")}
-                  >
-                    Spanish
-                  </li>
-                  <li
-                    className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-                    onClick={() => switchLanguage("fr")}
-                  >
-                    French
-                  </li>
-                  <li
-                    className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-                    onClick={() => switchLanguage("it")}
-                  >
-                    Italian
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-
-          {/* Theme Toggle Button */}
-          <button
-            onClick={toggleTheme}
-            aria-label="Toggle Theme"
-            className="transition hover:text-teal-400 focus:outline-none"
-          >
-            {theme === "light" ? <Moon size={24} /> : <Sun size={24} />}
-          </button>
-
-          {/* Search Icon */}
-          <button
-            onClick={toggleSearch}
-            aria-label="Search"
-            className="transition hover:text-teal-400 focus:outline-none"
-          >
-            <FiSearch size={24} />
-          </button>
-
-          {/* Mobile Menu Toggle */}
-          <button
-            onClick={toggleNavbar}
-            aria-label="Toggle Mobile Menu"
-            className="transition lg:hidden hover:text-teal-400 focus:outline-none"
-          >
-            {mobileDrawerOpen ? <X size={28} /> : <Menu size={28} />}
-          </button>
-        </div>
-      </div>
-
-      {/* Mobile Drawer */}
-      {mobileDrawerOpen && (
-        <div className="absolute left-0 w-full text-white bg-gray-800 lg:hidden top-full">
-          <ul className="flex flex-col items-center p-4 space-y-4">
+        {!searchOpen && (
+          <ul className="hidden space-x-8 lg:flex">
             {["home", "about", "research", "publications", "cv"].map((key) => (
               <li key={key}>
-                <Link
-                  to={`/${key}`}
-                  onClick={toggleNavbar} // Close drawer on link click
-                  className="transition hover:text-teal-400"
-                >
-                  {capitalizeFirstLetter(t(key))}
+                <Link to={`/${key}`} className="transition hover:text-teal-400">
+                  {capitalizeFirstLetter(t(key))} {/* Capitalize translation */}
                 </Link>
               </li>
             ))}
           </ul>
-          {/* Language Selector in Mobile Drawer */}
-          <div className="p-4 text-center">
-            <div className="relative inline-block">
+        )}
+
+        {/* Icons Section */}
+        <div className="flex items-center space-x-6">
+          {/* Language Selector */}
+          {!searchOpen && (
+            <div className="relative hidden lg:block">
               <button
                 onClick={() => setLanguageMenuOpen(!languageMenuOpen)}
                 aria-label="Language Selector"
@@ -189,7 +102,78 @@ const Navbar = () => {
                 </div>
               )}
             </div>
+          )}
+
+          {/* Theme Toggle Button */}
+          {!searchOpen && (
+            <button
+              onClick={toggleTheme}
+              aria-label="Toggle Theme"
+              className="transition hover:text-teal-400 focus:outline-none"
+            >
+              {theme === "light" ? <Moon size={24} /> : <Sun size={24} />}
+            </button>
+          )}
+
+          {/* Search Icon / Full-width Search Bar */}
+          <div className="relative flex-grow">
+            {searchOpen ? (
+              <div className="flex items-center justify-between w-full">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  className="w-full px-4 py-2 text-black bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-teal-300"
+                />
+                <button
+                  onClick={toggleSearch}
+                  aria-label="Close Search"
+                  className="ml-2 text-white hover:text-teal-400"
+                >
+                  <X size={24} />
+                </button>
+              </div>
+            ) : (
+              <button
+                onClick={toggleSearch}
+                aria-label="Search"
+                className="transition hover:text-teal-400 focus:outline-none"
+              >
+                <FiSearch size={24} />
+              </button>
+            )}
           </div>
+
+          {/* Mobile Menu Toggle */}
+          {!searchOpen && (
+            <button
+              onClick={toggleNavbar}
+              aria-label="Toggle Mobile Menu"
+              className="transition lg:hidden hover:text-teal-400 focus:outline-none"
+            >
+              {mobileDrawerOpen ? <X size={28} /> : <Menu size={28} />}
+            </button>
+          )}
+        </div>
+      </div>
+
+      {/* Mobile Drawer */}
+      {mobileDrawerOpen && !searchOpen && (
+        <div className="absolute left-0 w-full text-white bg-gray-800 lg:hidden top-full">
+          <ul className="flex flex-col items-center p-4 space-y-4">
+            {["home", "about", "research", "publications", "cv"].map((key) => (
+              <li key={key}>
+                <Link
+                  to={`/${key}`}
+                  onClick={toggleNavbar} // Close drawer on link click
+                  className="transition hover:text-teal-400"
+                >
+                  {capitalizeFirstLetter(t(key))}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       )}
     </nav>
